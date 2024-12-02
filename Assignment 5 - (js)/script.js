6eb7d347022979d2f32cb36293302a4274c4c438
Prodaaron <aaronmesfin60@gmail.com>
2024-12-02 19:51:23 +0300

    complete assignment

--- Assignment 5 - (js)/script.js
+++ Assignment 5 - (js)/script.js
@@ -3,7 +3,7 @@ const redDiv = document.createElement('div');
 redDiv.style.backgroundColor = 'red';
 redDiv.style.padding = '10px';
 
-redDiv.classList.add('.center');
+redDiv.classList.add('center');
 
 document.body.appendChild(redDiv);
 
@@ -20,3 +20,38 @@ redDiv.appendChild(image);
 const h1Tag = document.createElement('h1');
 
 h1Tag.textContent = 'Hi everyone!';
+document.body.append(h1Tag);
+
+
+const pa = document.createElement('p')
+
+document.body.append(pa);
+
+const span = document.createElement('span');
+pa.appendChild(span);
+const strong = document.createElement('strong');
+
+strong.textContent = 'this is a strong text!';
+
+span.appendChild(strong);
+
+redDiv.id = 'div-id';
+
+const span2 = document.createElement('span');
+span2.id = 'span2';
+span2.textContent = 'span tag';
+redDiv.appendChild(span2);
+const span3 = document.createElement('span');
+span3.id = 'span3';
+redDiv.appendChild(span3);
+span3.textContent = 'span tag';
+const span4 = document.createElement('span');
+
+span4.textContent = 'span tag';
+span4.id = 'span2';
+redDiv.appendChild(span4);
+
+document.getElementById('span3').remove();
+
+span2.style.color = 'darkred';
+span4.style.color = 'darkred';
